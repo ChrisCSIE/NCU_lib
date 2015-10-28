f65257cf6b0b10798b2788b3dbe1b01032522bc2
ChrisCSIE <chrisu00@hotmail.com>
2015-10-28 23:00:15 +0800

    Merged

--- src/ncu/lib/LoginActivity.java
+++ src/ncu/lib/LoginActivity.java
@@ -1,19 +1,26 @@
 package ncu.lib;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
 	Button login, clear;
 	EditText account, pwd;
 	ImageView welcome;
+	private static Boolean isExit = false;
+	private static Boolean hasTask = false;
 //	LinearLayout accountLinearLayout, pwdLinearLayout;
 
     @Override
@@ -73,4 +80,33 @@ public class LoginActivity extends Activity {
 //        }
 //        return super.onOptionsItemSelected(item);
 //    }
+    Timer timerExit = new Timer();
+	TimerTask task = new TimerTask() {
+		@Override
+		public void run() {
+			isExit = false;
+			hasTask = true;
+		}
+	};
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// 判斷是否按下Back
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// 是否要退出
+			if (isExit == false) {
+				isExit = true; // 記錄下一次要退出
+				Toast.makeText(this, "再按一次Back退出APP", Toast.LENGTH_SHORT).show();
+
+				// 如果超過兩秒則恢復預設值
+				if (!hasTask) {
+					timerExit.schedule(task, 2000);
+				}
+			} else {
+				finish(); // 離開程式
+				System.exit(0);
+			}
+		}
+		return false;
+	}
 }
